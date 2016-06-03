2184866cc6b85319407d4c4bb5c6bec83a0d8cf3
Jeff <medders.jeffrey@gmail.com>
2016-06-03 15:50:21 -0600

    hopefully fixed env stuff

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ done(null, user);
 passport.use(new LinkedInStrategy({
   clientID: process.env.LINKEDIN_CLIENT_ID,
   clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-  callbackURL: process.env.HOST + "http://localhost:3000/auth/linkedin/callback",
+  callbackURL: process.env.HOST + "/auth/linkedin/callback",
   scope: ['r_emailaddress', 'r_basicprofile'],
 }, function(accessToken, refreshToken, profile, done) {
   // asynchronous verification, for effect...
